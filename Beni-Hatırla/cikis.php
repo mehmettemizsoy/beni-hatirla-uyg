39630a1a9406a6ee0ca3af843695bd27750a2d6e
Mehmet Temizsoy <mehmettemizsoy45@gmail.com>
2023-04-09 18:38:38 +0300

    beni hatırla uyg

---
+++ Beni-Hatırla/cikis.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+session_destroy();
+Header("Location:index.php?durum=cikis");
+
+
+
+
+
+
+
+?>
---
+++ Beni-Hatırla/islem.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+
+if(isset($_POST['kullanici_giris'])){
+    
+if($_POST['kadi']=="edukey" && $_POST['pass']=="123456"){
+   
+    $_SESSION['kadi']=$_POST['kadi'];
+    $_SESSION['pass']=$_POST['pass'];
+
+    if(isset($_POST['beni_hatirla'])){
+//cookie atama işlemleri
+
+setcookie("kadi","edukey",strtotime("+1 day"));
+setcookie("pass","123456",strtotime("+1 day"));
+
+
+    }else{
+//cookie sil...
+setcookie("kadi","edukey",strtotime("-1 day"));
+setcookie("pass","123456",strtotime("-1 day"));
+
+    }
+    header("Location:index.php?durum=true");
+exit;
+} else{
+//giriş bilgileri doğru değilse
+header("Location:index.php?durum=false");
+exit;
+}
+
+
+
+
+} 
+
+
+?>
